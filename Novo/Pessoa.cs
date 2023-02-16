d95bf0e9896fec95cd10ed99f70b4632e5bc6088
estresadinha <120022356+estresadinha@users.noreply.github.com>
2023-02-16 11:51:40 -0300

    Add files via upload

---
+++ Novo/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novo
+{
+    public class Pessoa
+    {
+        public string nome;
+        public int idade;
+        public double altura;
+        public double peso;
+        public double imc;
+        public int anoatual;
+        public int anonac;
+
+        public void CalculaIMC(double peso,double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+            imc = this.peso / (this.altura* this.altura);
+        }
+
+        public void CalculaIdade(int anoatual,int anonac)
+        {
+            this.anoatual = anoatual;
+            this.anonac = anonac;
+            idade = this.anoatual-this.anonac;
+
+        }
+      
+
+    }
+      
+}
---
+++ Novo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            Console.WriteLine("digite seu nome ");
+            pessoa1.nome = Console.ReadLine();
+           
+
+            Console.WriteLine("digite sua altura ");
+            pessoa1.altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("digite seu peso ");
+            pessoa1.peso = Convert.ToDouble(Console.ReadLine());
+
+
+            pessoa1.CalculaIMC(pessoa1.peso,pessoa1.altura);
+            Console.WriteLine("o imc de " + pessoa1.nome+ "é " + pessoa1.imc);
+
+
+            Console.WriteLine("digite ano atual ");
+            pessoa1.anoatual= Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("digite ano nascimento ");
+            pessoa1.anonac = Convert.ToInt32(Console.ReadLine());
+
+            pessoa1.CalculaIdade (2023,pessoa1.anonac);
+            Console.WriteLine("idade  " + pessoa1.idade + " anos " );
+
+
+        }
+        
+
+    }
+}
